11685699c210570232c9d4668c2b4a6832c76d43
sinle <sinle@DESKTOP-RV9FV71>
2022-11-20 12:25:02 +0700

    fix jquery language

---
+++ jquery_ajax/sample.js
@@ -0,0 +1,23 @@
+$.ajax('${url}', {
+    type: '${method}',  // http method
+    data: ${ data },  // data to submit
+    success: function (data, status, xhr) {
+        //TODO: Handle success
+    },
+    error: function (jqXhr, textStatus, errorMessage) {
+        //TODO: Handle error
+    }
+});
+
+/***************************************************************************/
+
+$.ajax('${url}', {
+    type: '${method}',  // http method
+    data: ${ data },  // data to submit
+    success: function (data, status, xhr) {
+        //TODO: Handle success
+    },
+    error: function (jqXhr, textStatus, errorMessage) {
+        //TODO: Handle error
+    }
+});
